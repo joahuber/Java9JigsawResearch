2e0c75517d12e4298c93bc22a2b7920d5c29ede2
Marco Poli <marco.poli@bison-group.com>
2016-03-30 16:15:20 +0200

    Added unit tests that use degraph

---
+++ moduledemo/layer/src/test/java/ch/bbv/moduledemo/DegraphCheck.java
@@ -0,0 +1,23 @@
+package ch.bbv.moduledemo;
+
+import de.schauderhaft.degraph.check.ConstraintBuilder;
+
+import org.junit.Test;
+
+import static de.schauderhaft.degraph.check.Check.classpath;
+import static de.schauderhaft.degraph.check.JCheck.violationFree;
+import static org.junit.Assert.assertThat;
+
+public class DegraphCheck {
+
+  @Test
+  public void checkForCycles() {
+    ConstraintBuilder parts = classpath() //
+        .including("ch.bbv.moduledemo.**") //
+        .noJars() //
+        .withSlicing("part", "ch.bbv.moduledemo.(*).**")//
+        .allow("infrastructure", "adapter", "application", "domain") //
+        .printTo("build/degraph.graphml");
+    assertThat(parts, violationFree());
+  }
+}
